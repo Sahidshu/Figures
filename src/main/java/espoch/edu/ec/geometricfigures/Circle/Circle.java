6ab661c70201fda4904d9ef29a9ee003a58b5706
CristhiAn-555 <cristhianvalencia999@gmail.com>
2024-11-27 23:12:35 -0500

    hola

---
+++ src/main/java/espoch/edu/ec/geometricfigures/Circle/Circle.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package espoch.edu.ec.geometricfigures.Circle;
+
+/**
+ *
+ * @author PERSONAL
+ */
+public class Circle {
+    public double radius;
+    
+    public double circleArea(){
+        double circArea= radius * 3.14159;
+        return circArea;
+    }
+    
+    public double circlePerimeter(){
+        double circPer= (3.14159) * radius;
+        return circPer;
+    }
+}
+    
+    
+    
+    
---
+++ src/main/java/espoch/edu/ec/geometricfigures/square/Square.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package espoch.edu.ec.geometricfigures.square;
+
+/**
+ *
+ * @author PERSONAL
+ */
+public class Square {
+    public double sideLength;
+    
+    public double squareArea(){
+        double areaSq=(sideLength*sideLength);
+        return areaSq;
+}
+    public double squarePerimeter(){
+        double perSq=(sideLength*4);
+        return perSq;
+    }
+    
+
+    
+
+}
